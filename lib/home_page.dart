95c846a51c5357a577438ab7f49fbd53d906eda3
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2023-12-08 11:23:37 +0100

    first commit on flutter shop app

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Text(
+            'Shoes Collection',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+          )
+        ],
+      ),
+    ));
+  }
+}
